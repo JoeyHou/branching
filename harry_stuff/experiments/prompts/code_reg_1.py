3d586611c310554a4040381edba430896f09e1f3
Joey Hou <joeyhou.work@gmail.com>
2023-09-20 13:35:03 -0400

    removed unrelated files

--- harry_stuff/experiments/prompts/code_reg_1.py
+++
@@ -1,8 +0,0 @@
-def choose_best_option(goal, step, rationale):
-    # goal: "watch a movie at the cinema"
-    # step: "park the car"
-    options = ["park on the street", "park at a garage"]
-    if rationale == "save money":
-        return options[0]
-    elif rationale == "convenient":
-        return options[1]
